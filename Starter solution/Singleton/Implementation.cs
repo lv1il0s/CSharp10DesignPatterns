6b6b4c5ee53bd8468f2acfa73c9de04fee3050f0
lv1il0s <m.milos@protonmail.com>
2023-08-07 23:49:26 +0200

    Singleton - Implement the pattern naively

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -1,4 +1,35 @@
 ﻿namespace Singleton
 {
-  
+    public class Logger
+    {
+        private static Logger? _instance;
+
+
+        protected Logger()
+        {
+
+        }
+
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// SingletonOperation
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
 }
