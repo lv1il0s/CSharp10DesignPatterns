71f5f7277b7db8359bf4dc9fea313b84985f9507
lv1il0s <m.milos@protonmail.com>
2023-08-07 23:52:28 +0200

    Singleton - Thread-safe approach with Lazy<T>

--- Starter solution/Singleton/Implementation.cs
+++ Starter solution/Singleton/Implementation.cs
@@ -2,8 +2,12 @@
 {
     public class Logger
     {
-        private static Logger? _instance;
+       
+        //private static Logger? _instance;
 
+        // Thread-safe approach - we pass action to be called on 1st access, each next time, that instance is returned.
+        // Lazy<T>
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
 
         protected Logger()
         {
@@ -15,11 +19,12 @@
         {
             get
             {
-                if (_instance == null)
-                {
-                    _instance = new Logger();
-                }
-                return _instance;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+                return _lazyLogger.Value;
             }
         }
 
