26dd504c72c975d8f0dab7b062ecfd1eb94e2063
lv1il0s <m.milos@protonmail.com>
2023-08-08 00:35:35 +0200

    Factory Method - Implementation

--- Starter solution/FactoryMethod/Implementation.cs
+++ Starter solution/FactoryMethod/Implementation.cs
@@ -1,4 +1,108 @@
 ﻿namespace FactoryMethod
 {
-   
+    /// <summary>
+    /// Product
+    /// </summary>
+   public abstract class DiscountService
+    {
+        public abstract int DiscountPercnetage { get; }
+
+        // Returns the actual type name, makes testing easier.
+        public override string ToString()
+        {
+            return GetType().Name;
+        }
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercnetage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "BE":
+                        return 20;
+                    default:
+                        return 10;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Concrete product
+    /// </summary>
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercnetage
+        {
+            get
+            {
+                // Each code returns same discount, but can be only used once. This check
+                // could be implemented IRL.
+                return 15;
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
 }
--- Starter solution/FactoryMethod/Program.cs
+++ Starter solution/FactoryMethod/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+var factories = new List<DiscountFactory>()
+{
+    new CodeDiscountFactory(Guid.NewGuid()),
+    new CountryDiscountFactory("BE")
+};
+
+foreach (var factory in factories)
+{
+    var discountService = factory.CreateDiscountService();
+    Console.WriteLine($"Percentage {discountService.DiscountPercnetage} coming from {discountService}");
+}
+
+Console.ReadKey();
